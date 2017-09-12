3287054c11f0c9ba3657dd654cd3dc1ea59a46b9
cmromeo <meocodemagnus@gmail.com>
2017-09-12 21:36:54 +0800

    Add simple Shelves and Search components with routing inside BookApp

---
+++ src/Search.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+
+    render() {
+        return (
+            <div>
+                Hi Search!
+            </div>
+        )
+    }
+}
+
+export default Search;
---
+++ src/Shelves.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Shelves extends Component {
+
+    render() {
+        return (
+            <div>
+                Hi Shelves!
+            </div>
+        )
+    }
+}
+
+export default Shelves;
