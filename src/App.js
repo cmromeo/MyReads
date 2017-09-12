19af23f81c824f1bf765abedb94b66f74462011a
cmromeo <meocodemagnus@gmail.com>
2017-09-12 21:52:49 +0800

    feat: Download books  after BookApp mounts

--- src/App.js
+++ src/App.js
@@ -3,8 +3,25 @@ import './App.css';
 import {Route} from 'react-router-dom'
 import Shelves from './Shelves'
 import Search from './Search'
+import * as BooksAPI from './BooksAPI'
 
 class BooksApp extends React.Component {
+    
+    state = {
+        books: [],
+    }
+
+    downloadBooks = () => {
+        BooksAPI.getAll().then((books) => {
+            this.setState({books});
+            console.log(this.state);
+        });
+    }
+
+    componentDidMount(){
+        this.downloadBooks();
+    }
+
     render() {
         return (
             <div className="app">
