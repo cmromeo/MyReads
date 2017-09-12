db16d237a80bd0e4c53497d3b3e22a66e1ca65b1
cmromeo <meocodemagnus@gmail.com>
2017-09-12 22:58:17 +0800

    Display book data in Shelf component

--- src/Shelf.js
+++ src/Shelf.js
@@ -4,8 +4,44 @@ class Shelf extends Component {
     render(){
         console.log("shelf contains: ", this.props.shelf)
         return(
-            <div>
-                Hello Shelf!
+            <div className="list-books-content">
+                <div>
+                    <div className="bookshelf">
+                        <h2 className="bookshelf-title">{this.props.title}</h2>
+                        <div className="bookshelf-books">
+                            <ol className="books-grid">
+                                {this.props.shelf.map((book) => {
+                                    return (
+                                    <li key={book.id}>
+                                        <div className="book">
+                                        <div className="book-top">
+                                            <div className="book-cover" 
+                                                style={{ width: 128, height: 170, 
+                                                    backgroundImage: `url(${book.imageLinks.thumbnail})`, 
+                                                    backgroundRepeat: "no-repeat",
+                                                    backgroundSize: "cover" }}></div>
+                                            <div className="book-shelf-changer">
+                                            <select value={book.shelf} >
+                                                <option value="wantToRead" disabled>Move to...</option>
+                                                <option value="currentlyReading" >Currently Reading</option>
+                                                <option value="wantToRead">Want to Read</option>
+                                                <option value="read">Read</option>
+                                                <option value="none">None</option>
+                                            </select>
+                                            </div>
+                                        </div>
+                                        <div className="book-title">{book.title}</div>
+                                        <div className="book-authors">
+                                            {book.authors.join(", ")}
+                                        </div>
+                                        </div>
+                                    </li>
+                                    )
+                                })}
+                            </ol>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
